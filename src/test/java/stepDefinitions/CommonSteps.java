2de2de418daddb9010b1273f29cb59b4d88f195d
Gábor Glück <gluckgabor@gmail.com>
2024-10-13 16:23:36 +0200

    Initial commit

--- src/test/java/stepDefinitions/CommonSteps.java
+++ src/test/java/stepDefinitions/CommonSteps.java
@@ -97,5 +97,66 @@ public class CommonSteps extends BaseClass {
 				.body("city", equalTo("Madison"))
 				.body("telephone", equalTo("6085551023"));
 	}
+
+	// Step definitions for updating an owner
+	@Given("I update an Owner to have following data and save it")
+		public void i_update_an_owner_to_have_following_data(DataTable dataTable) {
+			String baseUrl = "http://localhost:9966/petclinic/api/owners/" + createdOwnerId;
+
+			String firstName = "";
+			String lastName = "";
+			String address= "";
+			String city= "";
+			String telephone= "";
+
+			List<Map<String, String>> owners = dataTable.asMaps(String.class, String.class);
+
+			for (Map<String, String> owner : owners) {
+				firstName = owner.get("firstName");
+				lastName = owner.get("lastName");
+				address = owner.get("address");
+				city = owner.get("city");
+				telephone = owner.get("telephone");
+
+
+				String requestBody = String.format(
+					"{\"firstName\": \"%s\", \"lastName\": \"%s\", \"address\": \"%s\", \"city\": \"%s\", \"telephone\": \"%s\"}",
+					firstName, lastName, address, city, telephone
+				);
+
+			given()
+					.header("Content-Type", "application/json")
+					.body(requestBody)
+					.when()
+					.put(baseUrl)
+					.then()
+					.statusCode(200); // Typically, a 204 No Content response is returned for successful updates
+			}
+	}
+
+	// Step definitions for deleting an owner
+	@Given("I delete an Owner")
+	public void i_delete_an_owner() {
+		String baseUrl = "http://localhost:9966/petclinic/api/owners/" + createdOwnerId;
+
+		given()
+				.when()
+				.delete(baseUrl)
+				.then()
+				.statusCode(204); // Typically, a 204 No Content response is returned for successful deletions
+	}
+
+	@Then("I see owner is correctly missing from returned results")
+	public void i_see_owner_is_correctly_missing_from_returned_results() {
+		String baseUrl = "http://localhost:9966/petclinic/api/owners/" + createdOwnerId;
+
+		given()
+				.when()
+				.get(baseUrl)
+				.then()
+				.statusCode(404); // Expecting 404 Not Found since the owner has been deleted
+	}
+
+
 }
 
--- src/test/resources/features/placeValidations.feature
+++ src/test/resources/features/placeValidations.feature
@@ -1,4 +1,4 @@
-Feature:
+Feature:PetClinic CRUD features
 
 Scenario:2 Owners create and read test
 	Given I add an Owner with following data and save it
@@ -8,9 +8,9 @@ Scenario:2 Owners create and read test
 	Then I see owner is loaded back correctly
 
 Scenario:3 Owners update and read test
-		Given I update an Owner to have following data: <firstName>, <lastName>, <address>, <city>, <telephone> and save it
+		Given I update an Owner to have following data and save it
 			| firstName | lastName | address                | city      | telephone         |
-			| Ana       | Horvat   | Trg bana Jelacica 7    | Split     | +385 98 765 4321  |
+			| Anton       | Horvat   | Trg bana Jelacica 7    | Split     | +385 98 765 4321  |
 		When I refresh browser
 		Then I see owner is loaded back correctly
 
